54e13dce0acfcd66e1925a441ed50a39f400e987
novichkov <novichkov@smartsolutions-123.ru>
2019-09-04 15:33:11 +0400

    Adding UI-form uising pyqt5

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ from loguru import logger
 from data_parser import DataParser
 from plot_painter import PlotPainter
 
+from PyQt5 import QtWidgets, uic
+import sys
+
+
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi('serial_port.ui', self)
+        self.show()
+
 
 def main():
     config = configparser.ConfigParser()
@@ -40,8 +50,13 @@ def main():
             painter = PlotPainter()
             painter.paint(data_parser.get_results())
 
-            break
+
+def main_ui():
+    app = QtWidgets.QApplication(sys.argv)
+    window = Ui()
+    app.exec_()
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    main_ui()
