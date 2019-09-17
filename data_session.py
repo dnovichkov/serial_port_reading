25b124ecb28cb945cd17f40da2843d646001066e
novichkov <novichkov@smartsolutions-123.ru>
2019-09-17 18:06:28 +0400

    Adding reading in different thread

--- data_session.py
+++ data_session.py
@@ -1,39 +1,80 @@
-from threading import Thread
+import time
 
 import serial
+from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot, QThread
 from loguru import logger
+from serial import SerialException
 
 from data_parser import DataParser
 
 DEFAULT_PORT_SETTINGS = {'SERIAL_PORT': 'COM1', 'SERIAL_RATE': 9600}
 
 
-def run_read(serial_device: serial.Serial, session):
-    while True:
-        reading = serial_device.readline().decode('utf-8')
-        logger.debug(reading)
-        session.add_data(reading)
+class SerialWorker(QObject):
+    read_data_signal = pyqtSignal(str)
+
+    def __init__(self, port_settings):
+
+        super().__init__()
+        self.is_started = True
+        port = port_settings['SERIAL_PORT']
+        rate = port_settings['SERIAL_RATE']
+        logger.debug(f'Open port {port} with rate {rate}')
+        try:
+            self.serial_device = serial.Serial(port, rate)
+        except SerialException as e:
+            logger.error(f'Error while reading: {e}')
+
+    @pyqtSlot()
+    def run(self):
+        logger.debug(f'Run {self.is_started}')
+        while True and self.is_started:
+            logger.debug(f'Run in cycle {self.is_started}')
+            time.sleep(1)
+            reading = self.serial_device.readline().decode('utf-8')
+            logger.debug(reading)
+            self.read_data_signal.emit(reading)
+
+    @pyqtSlot()
+    def stop(self):
+        logger.debug('SerialWorker stop')
+        self.is_started = False
+        self.serial_device.close()
 
 
-class DataSession:
+class DataSession(QObject):
+    signal_start_background_job = pyqtSignal()
+    signal_stop_background_job = pyqtSignal()
+
     def __init__(self, plot_painter, port_settings=DEFAULT_PORT_SETTINGS):
+        super().__init__()
         self.data_parser = DataParser()
         self.plot_painter = plot_painter
-        self.port_settings = port_settings
-        self.reading_thread = None
+
+        self.worker = SerialWorker(port_settings)
+        self.thread = QThread()
+        self.worker.moveToThread(self.thread)
+        self.worker.read_data_signal.connect(self.add_data)
+
+        self.signal_start_background_job.connect(self.worker.run)
+        self.signal_stop_background_job.connect(self.worker.stop)
+
+    def start_background_job(self):
+        self.thread.start()
+        self.signal_start_background_job.emit()
 
     def run(self):
-        ser = serial.Serial(self.port, self.rate)
-        self.reading_thread = Thread(target=run_read, args=(ser, self))
-        self.reading_thread.start()
+        self.start_background_job()
 
+    @pyqtSlot()
     def add_data(self, sensor_data: str):
+        logger.debug(sensor_data)
         sensor_data = self.data_parser.parse(sensor_data)
         if sensor_data:
             self.plot_painter.add_data(sensor_data)
             # TODO: save to DB
 
     def stop(self):
-        if self.reading_thread:
-            self.reading_thread.join()
-            self.reading_thread = None
+        self.worker.stop()
+        self.thread.quit()
+        self.thread.wait(1000)
--- main.py
+++ main.py
@@ -5,14 +5,15 @@ import sys
 import matplotlib
 import serial
 import serial.tools.list_ports
-from PyQt5 import QtWidgets, uic, QtGui
+from PyQt5 import QtWidgets, QtGui
 from PyQt5.QtWidgets import QInputDialog
 from loguru import logger
+from serial import SerialException
 
 from data_parser import DataParser
+from data_session import DataSession
 from plot_canvas import MyDynamicMplCanvas
 from plot_painter import PlotPainter
-
 from serial_port import Ui_MainWindow
 
 CONFIG_FILENAME = 'settings.conf'
@@ -21,6 +22,7 @@ CONFIG_FILENAME = 'settings.conf'
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         self.is_played = False
+        self.data_session = None
         self.figure_canvas = MyDynamicMplCanvas()
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
@@ -52,10 +54,20 @@ class MainWindow(QtWidgets.QMainWindow):
             self.ui.record_button.setText("Play")
             self.is_played = False
             self.figure_canvas.stop()
+            self.data_session.stop()
+            self.data_session = None
         else:
             self.ui.record_button.setText("Stop")
             self.is_played = True
             self.figure_canvas.start()
+            self.data_session = DataSession(self, get_config_settings())
+            try:
+                self.data_session.run()
+            except SerialException as e:
+                logger.error(f'Ошибка при чтении данных: {e}')
+
+    def add_data(self, sensor_data):
+        pass
 
     def show_dialog(self):
         devices = [port.device for port in serial.tools.list_ports.comports()]
@@ -70,11 +82,22 @@ class MainWindow(QtWidgets.QMainWindow):
         text, ok = QInputDialog.getItem(None, "Port setting", "Choose port:", devices, 0, False)
 
         if ok and text:
+            logger.debug(f'Selected port is {str(text)}')
             config.set("DATA", "SERIAL_PORT", str(text))
             with open(CONFIG_FILENAME, "w") as config_file:
                 config.write(config_file)
 
+
 matplotlib.use("Qt5Agg")
+
+
+def get_config_settings():
+    config = configparser.ConfigParser()
+    config.read(CONFIG_FILENAME)
+    serial_port = config['DATA']['SERIAL_PORT']
+    serial_rate = config['DATA']['SERIAL_RATE']
+    port_settings = {'SERIAL_PORT': serial_port, 'SERIAL_RATE': serial_rate}
+    return port_settings
 
 
 def main():
