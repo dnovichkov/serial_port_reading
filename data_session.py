37cbcb569eba4e3a41e454d50daa6b045e5d6279
novichkov <novichkov@smartsolutions-123.ru>
2019-09-30 17:44:09 +0400

    Revert "Class reader for read line optimization"
    This reverts commit 8c3e9863495f06dfe558af566e9997e0e8ce508a.

--- data_session.py
+++ data_session.py
@@ -12,29 +12,6 @@ from data_parser import DataParser
 DEFAULT_PORT_SETTINGS = {'SERIAL_PORT': 'COM1', 'SERIAL_RATE': 9600}
 
 
-class ReadLine:
-    def __init__(self, s):
-        self.buf = bytearray()
-        self.s = s
-
-    def readline(self):
-        i = self.buf.find(b"\n")
-        if i >= 0:
-            r = self.buf[:i + 1]
-            self.buf = self.buf[i + 1:]
-            return r
-        while True:
-            i = max(1, min(2048, self.s.in_waiting))
-            data = self.s.read(i)
-            i = data.find(b"\n")
-            if i >= 0:
-                r = self.buf + data[:i + 1]
-                self.buf[0:] = data[i + 1:]
-                return r
-            else:
-                self.buf.extend(data)
-
-
 class SerialWorker(QObject):
     """
     Worker for non-blocking reading data from serial port.
@@ -58,8 +35,7 @@ class SerialWorker(QObject):
     def run(self):
         while self.is_started:
             if self.serial_device.is_open:
-                rl = ReadLine(self.serial_device)
-                line = rl.readline().decode('utf-8')
+                line = self.serial_device.readline().decode('utf-8')
                 logger.debug(line)
                 self.read_data_signal.emit(line)
 
