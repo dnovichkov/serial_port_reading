e5fa5042ccb8c677fa4b3c9e0ec280db4e32af62
novichkov <novichkov@smartsolutions-123.ru>
2019-10-02 16:22:28 +0400

    Adding platform info to logs

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import configparser
 import sys
+import platform
+
 
 import matplotlib
 import serial
@@ -25,7 +27,12 @@ def main_ui():
     app = QtWidgets.QApplication(sys.argv)
 
     logger.add("Serial_port_reading.log", rotation="500 MB", level=get_logger_level())
-    logger.debug('Application was started')
+    logger.debug('Application was started, version is 2019-10-01 15:16')
+    logger.debug(f"Machine: {platform.machine()}")
+    logger.debug(f"version: {platform.version()}")
+    logger.debug(f"platform: {platform.platform()}")
+    logger.debug(f"uname: {platform.machine()}")
+    logger.debug(f"system: {platform.system()}")
     devices = [port.device for port in serial.tools.list_ports.comports()]
     logger.debug(f'Possible ports are: {devices}')
     win = MainWindow()
